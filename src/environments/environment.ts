b54ab81678aeb704c6f439479a35b636ff96cbe7
Shriram Naik <73263499+Shriram-11@users.noreply.github.com>
2025-03-24 18:01:46 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://172.16.10.218:5000'  // adjust this URL according to your backend
+  apiBaseUrl: 'localhost'  // adjust this URL according to your backend
 };
