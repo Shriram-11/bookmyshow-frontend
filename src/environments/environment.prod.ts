e32e30590e044bf9e31a5511c96460c17f46d1d1
Shriram Naik <73263499+Shriram-11@users.noreply.github.com>
2025-04-05 13:39:32 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'localhost'  // adjust this URL according to your backend
+  environmentName: 'production',
+  APP_ID: "book-my-movie-prod",
 };
